f099625055d0305ffb016afb3f839920e46d8a4a
mmuniz75 <mmunizs1975@gmail.com>
2020-05-05 14:01:03 -0300

    melhoria de login

--- services/messenger.js
+++ services/messenger.js
@@ -1,24 +1,46 @@
 const sulla = require('sulla');
+const fs = require('fs');
 
 const clients = []
+
+const sleep = async(ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 const login = async (phone) => {
 
     try{
-        let buffer;
-        client = await sulla.create(phone, (base64Qr, asciiQR) => {
-                            console.log(asciiQR);
-                            base64Qr = base64Qr.replace('data:image/png;base64,', '');
-                            buffer =Buffer.from(base64Qr, 'base64');
-                            });
 
-        clients[phone] = client;     
+       client = await getClient(phone);
+       if(client) 
+            return;
+             
+       let buffer;
+       let finished = false
+       sulla.create(phone, (base64Qr, asciiQR) => {
+                    console.log(asciiQR);
+                    base64Qr = base64Qr.replace('data:image/png;base64,', '');
+                    buffer =Buffer.from(base64Qr, 'base64');
+                    }, {
+                        refreshQR : 0
+                    })
+        .then(client => {
+                clients[phone] = client
+                finished = true
+            }    
+        );
+
+        while(!buffer && !finished)            
+            await sleep(1000)
+
         return buffer;
+
     }catch(e) {
         throw new Error(e.message);
     };
 
 };
+
 
 const send = async (phone,message,groups) => {
 
@@ -28,7 +50,7 @@ const send = async (phone,message,groups) => {
             throw new Error("403");
         else {
             await client.sendText(groups, message);
-            //client.close()    
+            client.close()    
         }    
           
     }catch(e) {
@@ -37,11 +59,20 @@ const send = async (phone,message,groups) => {
 };
 
 const getClient = async (phone) => {
-    if (!clients[phone]) { 
-        client = await sulla.create(phone);
-        clients[phone] = client
+    let client = clients[phone];
+    let connected = true;
+    if (!client) { 
+        if (fs.existsSync('./' + phone)) {
+            client = await sulla.create(phone);
+            clients[phone] = client
+        }else
+            connected = false;    
+    }else {
+        if(await !client.isConnected)
+            connected = false;
     }
-    return clients[phone];    
+
+    return connected?client:undefined;    
 }
 
 module.exports.login = login;
